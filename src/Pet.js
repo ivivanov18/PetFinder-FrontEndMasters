21085117daa88e8fcfffb890428e2f489bf5e4dd
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-01-08 23:39:55 +0200

    refactor: App into Results
    feat: fetch and list data from API

--- src/Pet.js
+++ src/Pet.js
@@ -3,7 +3,7 @@ import React from "react";
 const Pet = props => (
   <div id="my-id">
     <h1>{props.name.toUpperCase()}</h1>
-    <h2>{props.type}</h2>
+    <h2>{props.animal}</h2>
     <h3>{props.breed}</h3>
   </div>
 );
