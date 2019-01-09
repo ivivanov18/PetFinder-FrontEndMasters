8138a3970af1942057a8b4314c1b3b9d2fc524b5
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-01-09 08:59:02 +0200

    feat: implement Details component to fetch and display data

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router/lib/history";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: ""
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed: breed,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error });
+        navigate("/");
+      });
+  }
+
   render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
     return (
-      <div>
-        <h1>Details</h1>
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <p>{description}</p>
+        </div>
       </div>
     );
   }
